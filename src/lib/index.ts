c729c28250135d12e7c618e06f97c235e8ad1a5e
james <james@jmcdowall.com>
2025-08-07 02:08:55 +0000

    Fixed items that get included in build.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,7 @@
 export { skRoutes } from './skRoutes.svelte.js';
 export type { RouteConfig } from './skRoutes.svelte.js';
 
+export { skRoutesServer } from './skRoutes-server.js';
+export type { ServerRouteConfig } from './skRoutes-server.js';
+
 export { skRoutesPlugin } from './vite-plugin-skroutes.js';
